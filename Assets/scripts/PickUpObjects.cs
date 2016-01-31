c65f6a016bd9b71a7743e1463627070430d3d2cb
Kai-Lin Chuang <addy_chuang@hotmail.com>
2016-01-30 21:54:18 -0500

    Test model/animation for Sleepy, some scripts 追加されました!
    Currently scripting left and right swatting

---
+++ Assets/scripts/PickUpObjects.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpObjects : MonoBehaviour {
+	GameObject grabbedObject;
+	float grabbedObjectSize;
+	bool grab = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	GameObject GetMouseHoverObject(float range){
+		Vector3 position = gameObject.transform.position;
+		RaycastHit raycastHit;
+		Vector3 target = position + Camera.main.transform.forward * range;
+
+		if (Physics.Linecast (position, target, out raycastHit))
+			return raycastHit.collider.gameObject;
+		return null;
+
+
+	}
+
+	void TryGrabObject(GameObject grabObject){
+		if (grabObject == null)
+			return;
+
+		grabbedObject = grabObject;
+		grabbedObjectSize = grabObject.GetComponent<Renderer>().bounds.size.magnitude;
+	}
+
+	void DropObject(){
+		if (grabbedObject == null)
+			return;
+		grabbedObject = null;
+	}
+		
+
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (GetMouseHoverObject (5));
+
+		if (Input.GetMouseButtonDown(1)){
+			TryGrabObject(GetMouseHoverObject(5));
+			//grab = true;
+		}
+		else if (Input.GetMouseButtonDown(0)){
+			DropObject();
+		}
+			
+		if (grabbedObject != null) {
+			Vector3 newPosition = gameObject.transform.position + Camera.main.transform.forward * grabbedObjectSize;
+			grabbedObject.transform.position = newPosition;
+		}
+
+	}
+}
---
+++ Assets/scripts/SleepySwat.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SleepySwat : MonoBehaviour {
+
+	//public Animation anim;
+	void Start() {
+		
+	}
+	void Update() {
+		if (Input.GetKeyDown ("Q")) {
+			GetComponent<Animation>().Play ("leftSwat");
+		}
+	}
+}
