9b11d30e5870ffa5d8debbf606ecc6f14c41d847
Jalbert,Joseph <joejalbert29@gmail.com>
2016-02-01 18:46:08 -0500

    Put sleepy in the networked scene. Not currently functioning correctly with 2 players.

---
+++ Assets/NetworkPlayerChoice.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetworkPlayerChoice : MonoBehaviour {
+
+	public GameObject[] characters;
+	private int currentI;
+
+	bool hasSwapped = false;
+
+	public void SetCharacter(int i)
+	{
+		GetComponent<NetworkManager> ().playerPrefab = characters [i];
+		currentI = i;
+	}
+
+	void Update()
+	{
+		if (GetComponent<NetworkManager> ().isNetworkActive && !hasSwapped) {
+			if (currentI == 0) {
+				SetCharacter (1);
+			} else {
+				SetCharacter (0);
+			}
+			hasSwapped = true;
+		}
+	}
+}
