804338e55196c9990019a7d136e90c25e4ac6f9d
Kai-Lin Chuang <addy_chuang@hotmail.com>
2016-02-01 10:32:24 -0500

    Implemented swats and ability to kill mosquito
    Not the best, but it works for now

---
+++ Assets/scripts/KillMosquito.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillMosquito : MonoBehaviour {
+
+	void OnCollisionEnter (Collision col){
+		if (col.gameObject.tag == "hand") {
+			Destroy (gameObject);
+			Debug.Log ("hit?");
+		}
+	}
+
+	void Update(){
+		
+	}
+}
--- Assets/scripts/SleepySwat.cs
+++ Assets/scripts/SleepySwat.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class SleepySwat : MonoBehaviour {
 
+	public Animation anim;
 	//public Animation anim;
 	void Start() {
 		
 	}
 	void Update() {
-		if (Input.GetKeyDown ("Q")) {
+		if (Input.GetKey("q")) {
 			GetComponent<Animation>().Play ("leftSwat");
 		}
+		if (Input.GetKey ("e")) {
+			GetComponent<Animation> ().Play ("rightSwat");
+		}
+		if (Input.GetKey ("q") && Input.GetKey ("e")) {
+			GetComponent<Animation> ().Play ("RLSwat");
+		}
+
 	}
 }
