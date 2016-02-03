6e9e6c13179e86f649f95918c2aa498d693f9910
Jalbert,Joseph <joejalbert29@gmail.com>
2016-02-03 16:52:33 -0500

    OHKO on SUCKSPOTS

---
+++ Assets/Suckspots.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Suckspots : MonoBehaviour {
+
+	public GameObject[] suckspots;
+	public int currentSuckspot;
+
+	void Start () 
+	{
+		SetNewSuckspot ();
+	}
+
+	public void SetNewSuckspot(){
+		currentSuckspot = Random.Range (0, suckspots.Length);
+		for (int i = 0; i < suckspots.Length; i++) 
+		{
+			if (i == currentSuckspot) {
+				suckspots [i].SetActive(true);
+			} else {
+				suckspots [i].SetActive(false);
+			}
+		}
+	}
+}
