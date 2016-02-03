f9c2cdbb1db649896b3678a31015221e781558ab
Jalbert,Joseph <joejalbert29@gmail.com>
2016-02-03 15:55:36 -0500

    Suckspots.

---
+++ Assets/Suckspot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Suckspot : MonoBehaviour {
+
+	public GameObject[] suckspots;
+	public int currentSuckspot;
+
+	void Start () 
+	{
+		SetNewSuckspot ();
+	}
+
+	public void SetNewSuckspot(){
+		currentSuckspot = Random.Range (0, suckspots.Length);
+		for (int i = 0; i < suckspots.Length; i++) 
+		{
+			if (i == currentSuckspot) {
+				suckspots [i].SetActive(true);
+			} else {
+				suckspots [i].SetActive(false);
+			}
+		}
+	}
+
+	void Update () 
+	{
+
+	}
+}
