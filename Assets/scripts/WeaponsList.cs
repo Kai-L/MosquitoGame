d7aca392ec4e36c01ff4d244c227d0c905448eac
JoeJalbert <joejalbert29@gmail.com>
2016-02-19 19:27:55 -0500

    Some menu junk. Created script for generating weapons in different spawn locations.

---
+++ Assets/scripts/WeaponsList.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WeaponsList : MonoBehaviour {
+
+    public int numOfWeapons;
+
+    public List<GameObject> Weapons;
+    public List<GameObject> AvailableWeapons;
+
+    public List<Transform> AvailableSpawns;
+
+    int seed = 42;
+
+	void Start () {
+        GenerateWeapons(numOfWeapons);
+	}
+
+    void GenerateWeapons(int num)
+    {
+        Random.seed = seed;
+
+        for(int i = 0; i < num; i++)
+        {
+            int randWeapon = (int)Random.Range(0, AvailableWeapons.Count - 1);
+            int randSpawn = (int)Random.Range(0, AvailableSpawns.Count - 1);
+            Debug.Log(randWeapon);
+            AvailableWeapons.Remove(AvailableWeapons[randWeapon]);
+            AvailableSpawns.Remove(AvailableSpawns[randSpawn]);
+            //Instantiate(AvailableWeapons[randWeapon], AvailableSpawn[randSpawn], Quaternion.identity);
+        }
+    }
+}
