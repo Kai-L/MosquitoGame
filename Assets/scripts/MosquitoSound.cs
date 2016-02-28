832018e217e1dcd7ab9a5760967a0b836c001015
Kai-Lin Chuang <addy_chuang@hotmail.com>
2016-02-28 15:57:50 -0500

    Added Test Scene for sound. A simple script for Mosquito Buzzing sound

---
+++ Assets/scripts/MosquitoSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MosquitoSound : MonoBehaviour {
+
+	public AudioClip mosquitoBuzz;
+
+	private AudioSource source;
+	private float volLowRange = 0.5f;
+	private float volHighRange = 1.0f;
+
+	// Use this for initialization
+	void Awake () {
+
+		source = GetComponent<AudioSource> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		source.PlayOneShot (mosquitoBuzz, 1F);
+	}
+}
