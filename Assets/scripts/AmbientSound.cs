b4c717657ab8a3d6e055b218a76e0827c6c7a759
Kai-Lin Chuang <addy_chuang@hotmail.com>
2016-02-29 23:48:42 -0500

    Radio Sound and Ambient Sound + scripts added

---
+++ Assets/scripts/AmbientSound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientSound : MonoBehaviour {
+
+	public AudioClip ambientSound;
+
+	private AudioSource source;
+
+	// Use this for initialization
+	void Awake () {
+		source = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		source.Play ();
+	}
+}
---
+++ Assets/scripts/RadioSound.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RadioSound : MonoBehaviour {
+
+	public AudioClip radio;
+
+	private AudioSource source;
+	// Use this for initialization
+	void Awake () {
+		source = GetComponent<AudioSource> ();
+	}
+
+	void OnCollisionEnter (Collision col){
+		if (source.isPlaying) {
+			source.Pause ();
+		} else {
+			source.Play ();
+		}
+	}
+		
+	void Update () {
+		/*if (Input.GetKeyDown (KeyCode.F)) {
+			if (source.isPlaying) {
+				source.Pause();
+			} else {
+				source.Play();
+			}
+		}*/
+	}
+}
