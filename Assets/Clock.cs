16478a6a9ee2e672b45c80ab948686b92917d30e
JoeJalbert <joejalbert29@gmail.com>
2016-02-29 20:56:14 -0500

    Clock script, takes 6 mins to reach 6 AM.

---
+++ Assets/Clock.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clock : MonoBehaviour {
+
+    public TextMesh text;
+
+    public int startingHour;
+    public int startingMinutes;
+
+    public int endingHour;
+    public int endingMinutes;
+
+    public int currentHour;
+    public int currentMinute;
+
+    public float rateOfTime;
+
+    void Start()
+    {
+        currentHour = startingHour;
+        currentMinute = startingMinutes;
+        text.text = startingHour.ToString("00") + ":" + startingMinutes.ToString("00");
+        StartCoroutine(TickSecond());
+    }
+
+	IEnumerator TickSecond()
+    {
+        currentMinute += 1;
+        if(currentMinute == 60)
+        {
+            currentMinute = 0;
+            currentHour += 1;
+        }
+        if(currentHour == 13)
+        {
+            currentHour = 1;
+        }
+        if(currentHour == endingHour)
+        {
+            Debug.LogWarning("Morning Has Come");
+            yield break;
+        }
+        text.text = currentHour.ToString("00") + ":" + currentMinute.ToString("00");
+        yield return new WaitForSeconds(rateOfTime);
+        StartCoroutine(TickSecond());
+    }
+}
