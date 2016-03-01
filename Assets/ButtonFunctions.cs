0f53164454c74e62ee99ba509df19fcee12d639c
Jalbert,Joseph <joejalbert29@gmail.com>
2016-03-01 18:05:16 -0500

    Connecting menus with button scripts.

---
+++ Assets/ButtonFunctions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ButtonFunctions : MonoBehaviour {
+
+	public void LoadMainMenu(){
+		SceneManager.LoadScene ("Menu_Main");
+	}
+
+	public void LoadLobby(){
+		SceneManager.LoadScene ("Menu_NetworkSelect");
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+
+	public void TempLoadGame(){
+		SceneManager.LoadScene ("Week8_Prototype");
+	}
+
+}
