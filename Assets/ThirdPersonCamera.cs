6db7f85d5ae1e50b4ef344b9b18ef860b47f392d
Jalbert,Joseph <joejalbert29@gmail.com>
2016-03-01 15:20:36 -0500

    Adding in new mosquito model and third person camera script.

---
+++ Assets/ThirdPersonCamera.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+	public Vector3 originalPosition;
+	public Vector3 closePosition;
+
+	public float camSpeed;
+	public float camMin;
+	public float camMax;
+
+	bool shouldMove;
+
+	public float currentDistance;
+
+	public Vector3 currentWall;
+
+	void Start(){
+		originalPosition = transform.localPosition;
+	}
+
+	void Update()
+	{
+		if (Vector3.Distance (transform.position, currentWall) > 20 && currentWall != Vector3.zero) {
+			shouldMove = false;
+		}
+
+		if (shouldMove) {
+			transform.localPosition = Vector3.MoveTowards (transform.localPosition, closePosition, camSpeed);
+		} else {
+			transform.localPosition = Vector3.MoveTowards (transform.localPosition, originalPosition, camSpeed);
+		}
+	}
+
+	void OnTriggerEnter(Collider c) 
+	{
+		shouldMove = true;
+		currentWall = c.transform.position;
+	}
+}
