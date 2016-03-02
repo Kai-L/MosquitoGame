bd495ad8dddb2156ea6be384e221a30b6c77057b
Burch,Ethan <ekb45@n1-141-130.dhcp.drexel.edu>
2016-03-02 17:15:40 -0500

    Scripts for hitting objects and swinging weapons. Some sounds attached to prefabs

---
+++ Assets/scripts/EnviroSounds.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnviroSounds : MonoBehaviour {
+
+	private AudioSource audioSource;
+
+	public AudioClip audioClip;
+
+	bool hasPlayed = false;
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void OnCollisionEnter (Collision c) {
+		if (!hasPlayed) {
+			audioSource.PlayOneShot(audioClip);
+			hasPlayed = true;
+		}
+	}
+
+	void Update () {
+
+	}
+}
