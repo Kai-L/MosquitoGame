94bf16514c35ef02f60b841b1581d67ee11023b0
ekburch <ethanburch@MacBook-Pro.local>
2016-03-05 14:15:42 -0500

    Added sounds, random volume to ambient sounds, attached a couple sounds.

--- Assets/scripts/AmbientSound.cs
+++ Assets/scripts/AmbientSound.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class AmbientSound : MonoBehaviour {
 
 	public AudioClip ambientSound;
+	public AudioClip citySound;
 
 	private AudioSource source;
+	private int randVolume;
+	private float randTime = 60;
+	private float timeCounter;
 
 	// Use this for initialization
 	void Awake () {
@@ -14,6 +18,31 @@ public class AmbientSound : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		source.Play ();
+		source.PlayOneShot(ambientSound);
+
+		if (timeCounter > randTime) {
+			randTime = Random.Range(0, 120);
+			timeCounter = 0;
+			VariableVolume();
+			source.PlayOneShot (citySound, randVolume);
+		}
+
+		timeCounter += Time.deltaTime;
+	}
+
+	void VariableVolume() {
+		randVolume = Random.RandomRange(0, 2);
+
+		switch (randVolume) {
+			case 0:
+				source.volume = 1f;
+				break;
+			case 1:
+				source.volume = 0f;
+				break;
+			case 2:
+				source.volume = .5f;
+				break;
+		}
 	}
 }
