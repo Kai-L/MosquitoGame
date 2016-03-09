699f2333ffee9e771dad1b616fb0f5699bf2ba96
Josh-Kohn <Josh@Joshs-iMac-2.local>
2016-03-09 00:03:22 -0500

    Ambient light changes.  Additions to the AdaptiveLight.cs.  Light Probes.

--- Assets/scripts/AdaptiveLight.cs
+++ Assets/scripts/AdaptiveLight.cs
@@ -10,10 +10,16 @@ public class AdaptiveLight : MonoBehaviour {
 	public Light directionalLight;
 
 	public Color nightColor = new Color(39, 55, 90);
-	public float nightIntensity = 0.58f;
+	public float nightIntensity = 1.36f;
+
+	public Color nightAmbient = new Color(39,43, 135);
+	public float nightAmbientIntensity =.52f;
 
 	public Color dawnColor = new Color (255, 255, 191);
 	public float dawnIntensity = 0.75f;
+
+	public Color dawnAmbient = new Color(163, 165, 52);
+	public float dawnAmbientIntesntiy =2.33f;
 
 	public Color endColor = new Color (255, 249, 154);
 	public float endIntensity = 1.53f;
@@ -33,6 +39,9 @@ public class AdaptiveLight : MonoBehaviour {
 
 		directionalLight.color = nightColor;
 		directionalLight.intensity = nightIntensity;
+		RenderSettings.ambientLight = nightAmbient;
+		RenderSettings.ambientIntensity = nightAmbientIntensity;
+
 
 		//StartCoroutine(TickLight ());
 	}
@@ -45,6 +54,8 @@ public class AdaptiveLight : MonoBehaviour {
 		{
 			directionalLight.color = Color.Lerp (directionalLight.color, dawnColor, currentTime / dawnTime);
 			directionalLight.intensity = Mathf.Lerp (directionalLight.intensity, dawnIntensity, currentTime / dawnTime);
+			RenderSettings.ambientLight = Color.Lerp (RenderSettings.ambientLight, dawnAmbient, currentTime / dawnTime);
+			RenderSettings.ambientIntensity = Mathf.Lerp (RenderSettings.ambientIntensity, dawnAmbientIntesntiy, currentTime / dawnTime);
 		} 
 		else 
 		{
