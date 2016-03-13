f72f93575f25bf71c02394a91f86610d4fdd605c
JoeJalbert <joejalbert29@gmail.com>
2016-03-13 12:59:15 -0400

    Changed lamp to flicker off it's nice i guess

--- Assets/Lamps.cs
+++ Assets/Lamps.cs
@@ -4,21 +4,44 @@ using System.Collections;
 public class Lamps : MonoBehaviour {
 
 	Vector3 originalPosition;
-	bool hasTurnedOff;
+	bool hasTurnedOff = false;
 
-	void Start(){
+	void Start()
+    { 
 		originalPosition = transform.position;
 	}
 
-	void Update(){
+	void Update()
+    {
 
-		if (hasTurnedOff) {
+		if (hasTurnedOff)
+        {
 			return;
-		} else if (Vector3.Distance (originalPosition, transform.position) > 30) {
-			transform.GetComponentInChildren<Light> ().enabled = false;
-			transform.FindChild ("lamp1").GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.black);
 		}
+        else if (Vector3.Distance (originalPosition, transform.position) > 30)
+        {
+            StartCoroutine(FlickerOff());
+        }
 
 	}
+
+    IEnumerator FlickerOff()
+    {
+        hasTurnedOff = true;
+        transform.GetComponentInChildren<Light>().enabled = false;
+        transform.FindChild("lamp1").GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.black);
+        yield return new WaitForSeconds(.4f);
+        transform.GetComponentInChildren<Light>().enabled = true;
+        transform.FindChild("lamp1").GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.red);
+        yield return new WaitForSeconds(.2f);
+        transform.GetComponentInChildren<Light>().enabled = false;
+        transform.FindChild("lamp1").GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.black);
+        yield return new WaitForSeconds(.2f);
+        transform.GetComponentInChildren<Light>().enabled = true;
+        transform.FindChild("lamp1").GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.red);
+        yield return new WaitForSeconds(.2f);
+        transform.GetComponentInChildren<Light>().enabled = false;
+        transform.FindChild("lamp1").GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.black);
+    }
 
 }
