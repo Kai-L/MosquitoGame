142a48ca96af18e76df7b862bb9bb692d83b10c7
Kai-Lin Chuang <addy_chuang@hotmail.com>
2016-03-13 21:21:26 -0400

    Added mosquito buzz. Made a script to mute mosquito buzz for mosquito player

---
+++ Assets/MuteMosquitoBuzz.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuteMosquitoBuzz : MonoBehaviour {
+
+	AudioSource audio;
+
+	void Start() {
+		audio = GetComponent<AudioSource>();
+	}
+
+	void Update() {
+
+		if (GameObject.FindGameObjectWithTag ("MosquitoCam")) {
+			audio.mute = true;
+		} else {
+			audio.mute = false;
+		}
+
+	}
+}
