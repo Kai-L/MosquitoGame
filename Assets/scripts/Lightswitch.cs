713c04dc927440a0e6e2ddfba61afca0391a5bdf
ekburch <ethanburch@MacBook-Pro-61.local>
2016-03-14 20:01:42 -0400

    Lightswitches have lights and MIGHT flip. Weapons have grab points.

--- Assets/scripts/Lightswitch.cs
+++ Assets/scripts/Lightswitch.cs
@@ -4,18 +4,20 @@ using System.Collections;
 public class Lightswitch : MonoBehaviour {
 
 	public GameObject light;
+	public GameObject lightSwitch;
 	public bool lightActive;
-
 
 	void OnCollisionEnter(Collision c)
 	{
 		lightActive = !lightActive;
 		light.SetActive (lightActive);
+		lightSwitch.transform.Rotate(Vector3.down * Time.deltaTime, 0, 0);
 	}
 
 	void OnControllerColliderHit(ControllerColliderHit c)
 	{
 		lightActive = !lightActive;
 		light.SetActive (lightActive);
+		lightSwitch.transform.Rotate(Vector3.down * Time.deltaTime, 0, 0);
 	}
 }
